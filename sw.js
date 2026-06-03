7ec273045018d5828ea1353ce01aac99f62cf69a
Maverick-Disco4 <m.damm1648@googlemail.com>
2026-06-03 21:11:00 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "frankreich-2026-v9-road-routing";
+const CACHE = "frankreich-2026-v10-multi-photo";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./trip-data.json", "./manifest.webmanifest"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
